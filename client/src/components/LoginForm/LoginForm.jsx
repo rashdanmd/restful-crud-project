ec27f2e406ed94c6f3270b205a669571840b490c
rashdanmd <rashdan.developer@gmail.com>
2023-02-18 21:07:11 +0000

    initial folder and c omponents setup

---
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <div>
+      <>Login Form</>
+      <>SignUp Form</>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../Navbar/Logo";
+import Profile from "../Navbar/Profile";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Logo />
+      <Profile />
+    </div>
+  );
+};
+
+export default Navbar;
